6a128a66b7271c47cf30c297342b17e79254c14c
Dragos Dascalita Haut <ddragosd@gmail.com>
2018-06-11 21:58:19 -0700

    Execute reload cmd when a new folder is created

--- sync/watcher.go
+++ sync/watcher.go
@@ -1,8 +1,9 @@
 package sync
 
 import (
-	"github.com/koyachi/go-term-ansicolor/ansicolor"
 	"log"
+
+	"github.com/koyachi/go-term-ansicolor/ansicolor"
 )
 
 // Watches the specific folder for changes
@@ -20,10 +21,11 @@ func WatchFolderRecursive(folder string, debug bool) <-chan string {
 		for {
 			select {
 			case file := <-watcher.Files:
-				done <- file
 				log.Println(ansicolor.IntenseBlack("FS Changed"), ansicolor.Underline(file))
+				done <- file
 			case folder := <-watcher.Folders:
 				log.Println(ansicolor.Yellow("Watching path"), ansicolor.Yellow(folder))
+				done <- folder
 			}
 		}
 	}()
